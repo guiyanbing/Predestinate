7ebecafb07afae45ee008ea7451b61d3108bbed2
xieyao <xieyao@quzhao.com>
2017-03-21 13:35:33 +0800

    BaseActivity标题、BaseViewPanel,设置页迁移

--- app/src/main/java/com/juxin/predestinate/module/logic/config/Constant.java
+++ app/src/main/java/com/juxin/predestinate/module/logic/config/Constant.java
@@ -15,4 +15,16 @@ public class Constant {
     public static final int MSG_ID_Heartbeat_Reply = 65532; //回送逻辑的心跳消息，客户端发送心跳，服务器回复该类型的消息，包体为0
     public static final int MSG_ID_Heartbeat = 65535;       //普通心跳消息id
     public static final long Heartbeat_Time = 60 * 1000;    //心跳时间间隔，60s
+
+    public static final String IS_SHOW_MESSAGE = "is_show_message";             // 是否显示过通知栏
+    public static final String SETTING_QUIT_MESSAGE = "setting_quit_message";   //是否进行锁屏弹窗，存储key及默认值
+    public static final Boolean SETTING_QUIT_MESSAGE_DEFAULT = true;
+    public static final String SETTING_MESSAGE = "setting_message";             //是否进行消息提示，存储key及默认值
+    public static final Boolean SETTING_MESSAGE_DEFAULT = true;
+    public static final String SETTING_VIBRATION = "setting_vibration";         //是否进行新消息震动提示，存储key及默认值
+    public static final Boolean SETTING_VIBRATION_DEFAULT = true;
+    public static final String SETTING_VOICE = "setting_voice";                 //是否进行新消息声音提示，存储key及默认值
+    public static final Boolean SETTING_VOICE_DEFAULT = true;
+    public static final String SETTING_SLEEP_MESSAGE = "setting_sleep_message"; //是否睡眠免打扰，存储key及默认值
+    public static final Boolean SETTING_SLEEP_MESSAGE_DEFAULT = true;
 }
---
+++ app/src/main/java/com/juxin/predestinate/ui/setting/UsersSetAct.java
@@ -0,0 +1,134 @@
+package com.juxin.predestinate.ui.setting;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+import com.juxin.library.log.PToast;
+import com.juxin.library.utils.DirUtils;
+import com.juxin.predestinate.R;
+import com.juxin.predestinate.module.logic.application.ModuleMgr;
+import com.juxin.predestinate.module.logic.base.BaseActivity;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * 设置
+ * Created YAO on 2016/5/27.
+ */
+public class UsersSetAct extends BaseActivity implements View.OnClickListener {
+    private TextView desc_clean;
+    private static String cache;
+    private MyHandler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.y1_users_set);
+        setBackView(R.id.back_view, "设置");
+        initView();
+    }
+
+
+    static class MyHandler extends Handler {
+        final WeakReference<UsersSetAct> mActivity;
+
+        MyHandler(UsersSetAct act) {
+            mActivity = new WeakReference<>(act);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            UsersSetAct act = mActivity.get();
+            switch (msg.what) {
+                case 1:
+                    act.desc_clean.setText(cache);
+                    break;
+                case 2:
+                    act.desc_clean.setText("缓存状态良好");
+                    if (cache.equals("缓存状态良好")) {
+                        PToast.showShort("缓存状态良好，不需要清理");
+                    } else {
+                        PToast.showShort("缓存已清除");
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void initView() {
+        handler = new MyHandler(this);
+        findViewById(R.id.rl_exit).setOnClickListener(this);
+        findViewById(R.id.rl_clean).setOnClickListener(this);
+        desc_clean = (TextView) findViewById(R.id.desc_clean);
+        findViewById(R.id.rl_feedback).setOnClickListener(this);
+        LinearLayout users_content_head = (LinearLayout) findViewById(R.id.users_content_head);
+        SetNoticePanel noticePanel = new SetNoticePanel(UsersSetAct.this);
+//        users_content_head.addView(noticePanel.getContentView());
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+//                cache = DirUtils.getFormtCacheSize();
+                handler.sendEmptyMessage(1);
+            }
+        }).start();
+    }
+
+
+    @Override
+    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.rl_exit://退出登录
+//                PickerDialogUtil.showSimpleAlertDialog(UsersSetAct.this, new SimpleTipDialog.ConfirmListener() {
+//                    @Override
+//                    public void onCancel() {
+//                    }
+//
+//                    @Override
+//                    public void onSubmit() {
+//                        ModuleMgr.getLoginMgr().removeCookie(UsersSetAct.this);
+//                        NotificationsUtils.cancelAll();//如果还有通知栏提示，在退出帐号的时候全部清掉
+//                        ModuleMgr.getLoginMgr().logout();
+//                        setResult(200);
+//                        finish();
+//                    }
+//                }, "确定退出登录吗？",R.color.text_zhuyao_black, "");
+//                break;
+//            case R.id.rl_clean://  清除缓存
+//                PickerDialogUtil.showSimpleTipDialogExt(UsersSetAct.this, new SimpleTipDialog.ConfirmListener() {
+//                    @Override
+//                    public void onCancel() {
+//                    }
+//
+//                    @Override
+//                    public void onSubmit() {
+//                        new Thread() {
+//                            public void run() {
+//                                Message msg = new Message();
+//                                try {
+//                                    DirUtils.clearCache();
+//                                    msg.what = 2;
+//                                } catch (Exception e) {
+//                                    e.printStackTrace();
+//                                    msg.what = -1;
+//                                }
+//                                handler.sendMessage(msg);
+//                            }
+//                        }.start();
+//                    }
+//                }, "根据缓存文件的大小，清理时间将持续几秒至几分钟不等，请耐心等待", "清除缓存", "取消", "清理", true, R.color.text_zhuyao_black);
+//                break;
+//            case R.id.rl_feedback://意见反馈客服
+////                UIShow.showPrivateChatAct(this, MailSpecialID.customerService.getSpecialID(), MailSpecialID.customerService.getSpecialIDName());
+////                UIShow.showFeedBackAct(this);
+//                UIShow.showAboutAct(this);
+//                break;
+//        }
+    }
+}
