629ca25adf7e538648dfb0a85baf6d9d3877e5d4
zhangjixin <zhangjixin@quzhao.com>
2017-05-23 19:17:41 +0800

    列表左滑优化

--- app/src/main/java/com/juxin/predestinate/module/logic/swipemenu/SwipeMenuView.java
+++ app/src/main/java/com/juxin/predestinate/module/logic/swipemenu/SwipeMenuView.java
@@ -8,77 +8,79 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import com.juxin.predestinate.R;
 
 public class SwipeMenuView extends LinearLayout implements OnClickListener {
-	private int position;
+    private int position;
 
-	private SwipeMenu mSwipeMenu;
-	private View mContentView;
-	private OnSwipeMenuViewClickListener mSwipeMenuViewClickListener;
+    private SwipeMenu mSwipeMenu;
+    private View mContentView;
+    private OnSwipeMenuViewClickListener mSwipeMenuViewClickListener;
 
 
-	public SwipeMenuView(SwipeMenu swipeMenu){
-		this(swipeMenu.getContext());
-		this.mSwipeMenu = swipeMenu;
-		init();
-	}
+    public SwipeMenuView(SwipeMenu swipeMenu) {
+        this(swipeMenu.getContext());
+        this.mSwipeMenu = swipeMenu;
+        init();
+    }
 
-	private SwipeMenuView(Context context) {
-		this(context, null);
-	}
+    private SwipeMenuView(Context context) {
+        this(context, null);
+    }
 
-	public SwipeMenuView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
+    public SwipeMenuView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-	private void init(){
-		LayoutParams params = new LayoutParams(dp2px(70), LayoutParams.MATCH_PARENT);
-		LinearLayout parent = new LinearLayout(getContext());
-		parent.setGravity(Gravity.CENTER);
-		parent.setOrientation(LinearLayout.VERTICAL);
-		parent.setBackgroundColor(getResources().getColor(R.color.theme_color_red));
-		parent.setLayoutParams(params);
-		parent.setOnClickListener(this);
-		addView(parent);
+    private void init() {
+        LayoutParams params = new LayoutParams(dp2px(70), this.mSwipeMenu.getViewHeight());
+        params.gravity = Gravity.BOTTOM;
+        LinearLayout parent = new LinearLayout(getContext());
+        parent.setGravity(Gravity.CENTER);
+        parent.setOrientation(LinearLayout.VERTICAL);
+        parent.setBackgroundColor(getResources().getColor(R.color.theme_color_red));
+        parent.setLayoutParams(params);
+        parent.setOnClickListener(this);
+        addView(parent);
 
-		parent.addView(createTitle());
-	}
+        parent.addView(createTitle());
+    }
 
-	private TextView createTitle(){
-		TextView tv = new TextView(getContext());
-		tv.setText(mSwipeMenu.getTitle());
-		tv.setTextSize(mSwipeMenu.getTitleSize());
-		tv.setGravity(Gravity.CENTER);
-		tv.setTextColor(mSwipeMenu.getTitleColor());
-		return tv;
-	}
+    private TextView createTitle() {
+        TextView tv = new TextView(getContext());
+        tv.setText(mSwipeMenu.getTitle());
+        tv.setTextSize(mSwipeMenu.getTitleSize());
+        tv.setGravity(Gravity.CENTER);
+        tv.setTextColor(mSwipeMenu.getTitleColor());
+        return tv;
+    }
 
-	private int dp2px(int dp) {
-		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
-				getContext().getResources().getDisplayMetrics());
-	}
+    private int dp2px(int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+                getContext().getResources().getDisplayMetrics());
+    }
 
-	public void setPosition(int position) {
-		this.position = position;
-	}
+    public void setPosition(int position) {
+        this.position = position;
+    }
 
-	@Override
-	public void onClick(View v) {
-		if (mSwipeMenuViewClickListener != null) {
-			mSwipeMenuViewClickListener.onSwipeMenuViewClick(position, mSwipeMenu, mContentView);
-		}
-	}
+    @Override
+    public void onClick(View v) {
+        if (mSwipeMenuViewClickListener != null) {
+            mSwipeMenuViewClickListener.onSwipeMenuViewClick(position, mSwipeMenu, mContentView);
+        }
+    }
 
-	public interface OnSwipeMenuViewClickListener {
-		void onSwipeMenuViewClick(int position, SwipeMenu swipeMenu, View contentView);
-	}
+    public interface OnSwipeMenuViewClickListener {
+        void onSwipeMenuViewClick(int position, SwipeMenu swipeMenu, View contentView);
+    }
 
-	public void setSwipeMenuViewClickListener(OnSwipeMenuViewClickListener swipeMenuViewClickListener) {
-		this.mSwipeMenuViewClickListener = swipeMenuViewClickListener;
-	}
+    public void setSwipeMenuViewClickListener(OnSwipeMenuViewClickListener swipeMenuViewClickListener) {
+        this.mSwipeMenuViewClickListener = swipeMenuViewClickListener;
+    }
 
-	public void setContentView(View contentView) {
-		this.mContentView = contentView;
-	}
+    public void setContentView(View contentView) {
+        this.mContentView = contentView;
+    }
 }
