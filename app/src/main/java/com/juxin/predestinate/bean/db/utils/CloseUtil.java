b9f6ee07ea8b71048590cc529c5a91364ee72d8d
zhangruipeng <sanchi.zhang@gmail.com>
2017-05-25 14:41:32 +0800

    badge. floating position.

--- app/src/main/java/com/juxin/predestinate/bean/db/utils/CloseUtil.java
+++ app/src/main/java/com/juxin/predestinate/bean/db/utils/CloseUtil.java
@@ -9,13 +9,14 @@ import java.io.IOException;
 @SuppressWarnings("WeakerAccess")
 public class CloseUtil {
 
-    private CloseUtil() { }
+    private CloseUtil() {
+    }
 
     public static void close(Closeable... params) {
         if (null != params) {
             try {
                 for (Closeable closeable : params) {
-                    closeable.close();
+                    if (closeable != null) closeable.close();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
