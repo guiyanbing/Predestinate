b84797fb177178231934e1719eb75a81b409800c
Kind <191350614@qq.com>
2017-05-25 17:26:05 +0800

    视频消息展示

--- app/src/main/java/com/juxin/predestinate/module/local/msgview/chatview/msgpanel/ChatPanelVideo.java
+++ app/src/main/java/com/juxin/predestinate/module/local/msgview/chatview/msgpanel/ChatPanelVideo.java
@@ -1,32 +1,30 @@
 package com.juxin.predestinate.module.local.msgview.chatview.msgpanel;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.text.Html;
 import android.widget.TextView;
-
 import com.juxin.predestinate.R;
 import com.juxin.predestinate.bean.center.user.light.UserInfoLightweight;
 import com.juxin.predestinate.module.local.chat.msgtype.BaseMessage;
-import com.juxin.predestinate.module.local.chat.msgtype.TextMessage;
 import com.juxin.predestinate.module.local.chat.msgtype.VideoMessage;
 import com.juxin.predestinate.module.local.msgview.ChatAdapter;
 import com.juxin.predestinate.module.local.msgview.chatview.ChatPanel;
+import com.juxin.predestinate.module.util.TimeUtil;
 
 /**
  * Created by Kind on 2017/5/12.
  */
 
 public class ChatPanelVideo extends ChatPanel {
-    private TextView chat_item_text;
+    private TextView chat_item_video_text;
 
     public ChatPanelVideo(Context context, ChatAdapter.ChatInstance chatInstance, boolean sender) {
-        super(context, chatInstance, R.layout.f1_chat_item_panel_text, sender);
+        super(context, chatInstance, R.layout.f1_chat_item_panel_audio, sender);
     }
 
     @Override
     public void initView() {
-        chat_item_text = (TextView) findViewById(R.id.chat_item_text);
+        chat_item_video_text = (TextView) findViewById(R.id.chat_item_video_text);
     }
 
     @Override
@@ -34,8 +32,12 @@ public class ChatPanelVideo extends ChatPanel {
         if (msgData == null || !(msgData instanceof VideoMessage)) {
             return false;
         }
-
-
+        VideoMessage videoMessage = (VideoMessage) msgData;
+        chat_item_video_text.setText(VideoMessage.getVideoChatContent(videoMessage.getEmLastStatus(), videoMessage.getTime(), videoMessage.isSender()));
         return true;
     }
+
+
+
+
 }
