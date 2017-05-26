28ad51c2c17c16f5bf4d7264c8b690bfa5e2b877
lc <lc@quzhao.com>
2017-05-26 19:09:02 +0800

    微调滚动条

--- app/src/main/java/com/juxin/predestinate/ui/user/my/view/GiftMessageInforView.java
+++ app/src/main/java/com/juxin/predestinate/ui/user/my/view/GiftMessageInforView.java
@@ -1,6 +1,7 @@
 package com.juxin.predestinate.ui.user.my.view;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,22 +14,46 @@ import com.juxin.predestinate.bean.my.GiftMessageList;
  * Created by zm on 2017/1/8
  */
 public class GiftMessageInforView extends LMarqueeFactory<LinearLayout, GiftMessageList.GiftMessageInfo> {
-
-    public GiftMessageInforView(Context mContext) {
+    private int mGender;
+    public GiftMessageInforView(Context mContext, int gender) {
         super(mContext);
+        this.mGender = gender;
     }
 
     @Override
     public LinearLayout generateMarqueeItemView(GiftMessageList.GiftMessageInfo data) {
         LinearLayout linearLayout = (LinearLayout) inflate(R.layout.f1_gift_message_info_view);
+        LinearLayout content = (LinearLayout) linearLayout.findViewById(R.id.content);
         TextView txt_one = (TextView) linearLayout.findViewById(R.id.txt_info_one);
         TextView txt_two = (TextView) linearLayout.findViewById(R.id.txt_info_two);
         TextView txt_three = (TextView) linearLayout.findViewById(R.id.txt_info_three);
         TextView txt_four = (TextView) linearLayout.findViewById(R.id.txt_info_four);
-        txt_one.setText(data.getFrom_name()+"");
-        txt_two.setText(R.string.just_now);
-        txt_three.setText(data.getTo_name()+"");
-        txt_four.setText(mContext.getString(R.string.sent)+data.getNum()+mContext.getString(R.string.ge)+data.getGname());
+        TextView txt_five = (TextView) linearLayout.findViewById(R.id.txt_info_five);
+        if(mGender == 1) {
+            content.setBackgroundColor(ContextCompat.getColor(mContext, R.color.lmarquee_boy_bg));
+            txt_one.setTextColor(ContextCompat.getColor(mContext, R.color.lmarquee_boy_nick));
+            txt_two.setTextColor(ContextCompat.getColor(mContext, R.color.white));
+            txt_three.setTextColor(ContextCompat.getColor(mContext, R.color.lmarquee_boy_nick));
+            txt_four.setTextColor(ContextCompat.getColor(mContext, R.color.lmarquee_boy_gift));
+            txt_five.setTextColor(ContextCompat.getColor(mContext, R.color.white));
+
+            txt_one.setText(data.getFrom_name()+"");
+            txt_two.setText(R.string.just_now);
+            txt_three.setText(data.getTo_name()+"");
+            txt_four.setText(data.getGname());
+            txt_five.setText(R.string.very_rich);
+        }else if(mGender == 2) {
+            content.setBackgroundColor(ContextCompat.getColor(mContext, R.color.lmarquee_girl_bg));
+            txt_one.setTextColor(ContextCompat.getColor(mContext, R.color.lmarquee_girl_nick_gift));
+            txt_two.setTextColor(ContextCompat.getColor(mContext, R.color.black));
+            txt_three.setTextColor(ContextCompat.getColor(mContext, R.color.lmarquee_girl_nick_gift));
+            txt_four.setTextColor(ContextCompat.getColor(mContext, R.color.black));
+
+            txt_one.setText(data.getTo_name()+"");
+            txt_two.setText(R.string.just_now_get);
+            txt_three.setText(data.getGname()+"");
+            txt_four.setText(R.string.very_greater);
+        }
         return linearLayout;
     }
 }
