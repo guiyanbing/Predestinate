a336822f903dfd2bfbaa523c7e7018ee52ddad17
IQQ <wbqlove1234@163.com>
2017-06-06 14:05:44 +0800

    转盘设置,vip弹框

--- app/src/main/java/com/juxin/predestinate/ui/mail/chat/PrivateChatAct.java
+++ app/src/main/java/com/juxin/predestinate/ui/mail/chat/PrivateChatAct.java
@@ -355,7 +355,7 @@ public class PrivateChatAct extends BaseActivity implements View.OnClickListener
         if (ModuleMgr.getCenterMgr().getMyInfo().isVip()) {
             UIShow.showCheckOtherInfoAct(this, whisperID);
         } else {
-            UIShow.showGoodsVipDlgOld(this);
+            UIShow.showGoodsVipDlgOld(this, 2);
         }
     }
 
