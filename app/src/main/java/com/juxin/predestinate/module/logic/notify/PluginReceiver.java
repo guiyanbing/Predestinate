ee30608a1acaf95407f188f5c079908071a54283
WeberGuo <websterguo@eyouv.cn>
2017-06-07 15:13:14 +0800

    增加和插件交互的广播，启动插件传入用户信息以及礼物列表

---
+++ app/src/main/java/com/juxin/predestinate/module/logic/notify/PluginReceiver.java
@@ -0,0 +1,16 @@
+package com.juxin.predestinate.module.logic.notify;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * @author gwz
+ * 接收插件广播
+ */
+public class PluginReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
