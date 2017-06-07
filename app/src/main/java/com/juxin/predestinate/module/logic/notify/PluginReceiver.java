65fcee83e49a6acaca0b001d1230f872b7e20f6c
WeberGuo <websterguo@eyouv.cn>
2017-06-07 17:10:17 +0800

    注册与插件交互的广播

--- app/src/main/java/com/juxin/predestinate/module/logic/notify/PluginReceiver.java
+++ app/src/main/java/com/juxin/predestinate/module/logic/notify/PluginReceiver.java
@@ -4,6 +4,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.juxin.predestinate.module.local.chat.ChatMgr;
+import com.juxin.predestinate.module.logic.application.ModuleMgr;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * @author gwz
  * 接收插件广播
@@ -11,6 +17,20 @@ import android.content.Intent;
 public class PluginReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        String json = intent.getStringExtra("extra_json");
+        if(json != null){
+            try {
+                JSONObject jo = new JSONObject(json);
+                switch (jo.optInt("mt")){
+                    case 1:
+                        ModuleMgr.getChatMgr().sendTextMsg(null,jo.getString("tid"),jo.getString("text"));
+                        break;
+                    case 3:
 
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
