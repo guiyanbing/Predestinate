b6d2481d6a184d77802c716a43ad633351586e8b
WeberGuo <websterguo@eyouv.cn>
2017-06-07 18:51:31 +0800

    调起插件，传入是否自己露脸的标识

--- app/src/main/java/com/juxin/predestinate/module/util/VideoAudioChatHelper.java
+++ app/src/main/java/com/juxin/predestinate/module/util/VideoAudioChatHelper.java
@@ -16,6 +16,7 @@ import com.juxin.predestinate.bean.center.user.detail.UserDetail;
 import com.juxin.predestinate.bean.config.VideoVerifyBean;
 import com.juxin.predestinate.module.logic.application.ModuleMgr;
 import com.juxin.predestinate.module.logic.baseui.custom.SimpleTipDialog;
+import com.juxin.predestinate.module.logic.config.Constant;
 import com.juxin.predestinate.module.logic.config.UrlParam;
 import com.juxin.predestinate.module.logic.model.impl.HttpMgrImpl;
 import com.juxin.predestinate.module.logic.model.mgr.HttpMgr;
@@ -249,6 +250,7 @@ public class VideoAudioChatHelper {
         bundle.putLong("vc_dst_uid", dstUid);
         bundle.putString("vc_self_info", PSP.getInstance().getString(INFO_SAVE_KEY, ""));
         bundle.putString("vc_gift_list",ModuleMgr.getCommonMgr().getGiftLists().getStrGiftConfig());
+        bundle.putInt("vc_own",PSP.getInstance().getInt(Constant.APPEAR_TYPE,1));//1看自己，2不看自己
         return bundle;
     }
 
