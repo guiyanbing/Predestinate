9166dd74903c4b51a30b6ca9a07012294a08d481
huangxiujun <huangxj88>
2017-06-07 14:23:03 +0800

    1.修复不能解锁视频的问题 2.私密视频详情里以解锁的视频不加模糊处理

--- app/src/main/java/com/juxin/predestinate/ui/user/util/VideoHoriAdapter.java
+++ app/src/main/java/com/juxin/predestinate/ui/user/util/VideoHoriAdapter.java
@@ -48,8 +48,11 @@ public class VideoHoriAdapter extends ExBaseAdapter<UserVideo> {
         }
 
         UserVideo userVideo = getItem(position);
-
-        ImageLoader.loadBlur(App.context, userVideo.getPic(), mHolder.img_media, 8);
+        if(userVideo.isCanView()){
+            ImageLoader.loadAvatar(App.context, userVideo.getPic(), mHolder.img_media);
+        }else{
+            ImageLoader.loadBlur(App.context, userVideo.getPic(), mHolder.img_media, 8);
+        }
         mHolder.img_shade.setVisibility(View.VISIBLE);
         return convertView;
     }
