39bb313f53517134101b8858227e5140b03ea9b6
lc <lc@quzhao.com>
2017-06-08 12:36:38 +0800

    单选（默认情况下也保存）

--- app/src/main/java/com/juxin/predestinate/ui/user/my/LookAtHerDlg.java
+++ app/src/main/java/com/juxin/predestinate/ui/user/my/LookAtHerDlg.java
@@ -79,17 +79,15 @@ public class LookAtHerDlg extends BaseDialogFragment implements View.OnClickList
                 cb_own_disagree.setChecked(true);
                 break;
             case R.id.tv_select_ok:
-                if (cb_def_sel.isChecked()) {
-                    if (cb_own_agree.isChecked()) {
+                if (cb_own_agree.isChecked()) {
+                    saveType(Constant.APPEAR_SINGLE_TYPE, Constant.APPEAR_TYPE_OWN);
+                    if (cb_def_sel.isChecked()) {
                         saveType(Constant.APPEAR_FOREVER_TYPE, Constant.APPEAR_TYPE_OWN);
-                    } else if (cb_own_disagree.isChecked()) {
-                        saveType(Constant.APPEAR_FOREVER_TYPE, Constant.APPEAR_TYPE_NO_OWN);
                     }
-                }else {
-                    if (cb_own_agree.isChecked()) {
-                        saveType(Constant.APPEAR_SINGLE_TYPE, Constant.APPEAR_TYPE_OWN);
-                    } else if (cb_own_disagree.isChecked()) {
-                        saveType(Constant.APPEAR_SINGLE_TYPE, Constant.APPEAR_TYPE_NO_OWN);
+                } else if (cb_own_disagree.isChecked()) {
+                    saveType(Constant.APPEAR_SINGLE_TYPE, Constant.APPEAR_TYPE_NO_OWN);
+                    if (cb_def_sel.isChecked()) {
+                        saveType(Constant.APPEAR_FOREVER_TYPE, Constant.APPEAR_TYPE_NO_OWN);
                     }
                 }
                 VideoAudioChatHelper.getInstance().inviteVAChat((Activity) context, otherId, VideoAudioChatHelper.TYPE_VIDEO_CHAT);
