4be0f6b838c1496b145c6c3e04121ccd221c11de
WeberGuo <websterguo@eyouv.cn>
2017-06-09 13:14:13 +0800

    统计代码

--- app/src/main/java/com/juxin/predestinate/ui/mail/chat/PrivateChatAct.java
+++ app/src/main/java/com/juxin/predestinate/ui/mail/chat/PrivateChatAct.java
@@ -30,6 +30,8 @@ import com.juxin.predestinate.module.local.mail.MailSpecialID;
 import com.juxin.predestinate.module.local.msgview.ChatViewLayout;
 import com.juxin.predestinate.module.local.msgview.chatview.ChatInterface;
 import com.juxin.predestinate.module.local.pay.CheckYCoinBean;
+import com.juxin.predestinate.module.local.statistics.SendPoint;
+import com.juxin.predestinate.module.local.statistics.Statistics;
 import com.juxin.predestinate.module.logic.application.ModuleMgr;
 import com.juxin.predestinate.module.logic.baseui.BaseActivity;
 import com.juxin.predestinate.module.logic.config.Constant;
@@ -309,6 +311,7 @@ public class PrivateChatAct extends BaseActivity implements View.OnClickListener
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.chat_title_attention: {//关注
+                Statistics.userBehavior(SendPoint.chatframe_nav_follow);
                 String content = ModuleMgr.getCenterMgr().getMyInfo().getNickname();
                 if (!TextUtils.isEmpty(content) && !"null".equals(content))
                     content = "[" + content + "]" + getString(R.string.just_looking_for_you);
@@ -341,12 +344,15 @@ public class PrivateChatAct extends BaseActivity implements View.OnClickListener
                 break;
             }
             case R.id.chat_title_phone://手机
+                Statistics.userBehavior(SendPoint.chatframe_nav_tel);
                 checkAndShowVip();
                 break;
             case R.id.chat_title_wx://微信
+                Statistics.userBehavior(SendPoint.chatframe_nav_weixin);
                 checkAndShowVip();
                 break;
             case R.id.chat_title_yb://Y币
+                Statistics.userBehavior(SendPoint.chatframe_nav_y);
                 UIShow.showGoodsYCoinDlgOld(this);
                 break;
             case R.id.cus_top_title_img_phone://音视频
