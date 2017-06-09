408c5cc3e8b5fb2475f7d46e7032f318afdb3f69
huangxiujun <huangxj88>
2017-06-09 14:10:14 +0800

    1.修复他人信息里 联系方式 的VIP与非VIP 显示不正确的问题 2.修复统计里打招呼崩溃问题

--- app/src/main/java/com/juxin/predestinate/module/local/statistics/StatisticsPayAndLoginAfter.java
+++ app/src/main/java/com/juxin/predestinate/module/local/statistics/StatisticsPayAndLoginAfter.java
@@ -14,8 +14,8 @@ public class StatisticsPayAndLoginAfter {
     //    公共组件-登录后引导
     //    登录后引导->一键打招呼,登录后引导(男用户)
     public static void sayHi(List<UserInfoLightweight> data) {
-        long[] sayhis = new long[4];
-        for (int i = 0; i < 4; i++) {
+        long[] sayhis = new long[data != null ?data.size():0];
+        for (int i = 0; i < sayhis.length; i++) {
             sayhis[i] = data.get(i).getUid();
         }
         Map<String, Object> parms = new HashMap<>();
