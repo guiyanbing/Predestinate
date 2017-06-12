f7ff832373085819a0383074b5dcf7372ae374da
Kind <191350614@qq.com>
2017-06-12 18:15:50 +0800

    greetNum

--- app/src/main/java/com/juxin/predestinate/ui/mail/MailFragmentAdapter.java
+++ app/src/main/java/com/juxin/predestinate/ui/mail/MailFragmentAdapter.java
@@ -82,8 +82,7 @@ public class MailFragmentAdapter extends ExBaseAdapter<BaseMessage> {
         baseMessage.setWhisperID(String.valueOf(MailMsgID.Greet_Msg.type));
         baseMessage.setWeight(MessageConstant.Max_Weight);
         baseMessage.setMailItemStyle(MailItemType.Mail_Item_Other.type);
-        PLogger.printObject("StrangerNew()=adapter=" + ModuleMgr.getChatListMgr().getStrangerNew());
-        baseMessage.setNum(ModuleMgr.getChatListMgr().getStrangerNew() ? 1 : 0);
+        baseMessage.setNum(ModuleMgr.getChatListMgr().getGreetNum());
         baseMessage.setName("打招呼的人");
         int geetNum = ModuleMgr.getChatListMgr().getGeetList().size();
         baseMessage.setAboutme(geetNum > 0 ? "共有" + geetNum + "位打招呼的人" : "暂时还没有打招呼的人");
