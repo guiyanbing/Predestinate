5292f9048b83f8ab2b1768f8779b6906d69a7bb5
zhangruipeng <sanchi.zhang@gmail.com>
2017-06-14 09:30:23 +0800

    plog printer.

--- library/src/main/java/com/juxin/library/log/PLogger.java
+++ library/src/main/java/com/juxin/library/log/PLogger.java
@@ -1,45 +1,52 @@
 package com.juxin.library.log;
 
-import android.os.Looper;
-import android.os.Process;
-import android.text.TextUtils;
-
+import com.elvishew.xlog.LogConfiguration;
 import com.elvishew.xlog.LogLevel;
 import com.elvishew.xlog.XLog;
+import com.elvishew.xlog.printer.AndroidPrinter;
+import com.elvishew.xlog.printer.Printer;
 
 /**
  * 全局统一的日志打印工具类：comes from <pre>https://github.com/elvishew/XLog</pre>
  * Created by ZRP on 2016/9/8.
  */
 public class PLogger {
-    private static final int STACK = 4;
 
     public static void init(boolean isDebug) {
-        XLog.init(isDebug ? LogLevel.ALL : LogLevel.NONE);
+        LogConfiguration config = new LogConfiguration.Builder()
+                .logLevel(isDebug ? LogLevel.ALL : LogLevel.NONE)       // 指定日志级别，低于该级别的日志将不会被打印，默认为 LogLevel.ALL
+                .tag("Predestinate")                                    // 指定 TAG，默认为 "X-LOG"
+                .t()                                                    // 允许打印线程信息，默认禁止
+                .st(3)                                                  // 允许打印深度为2的调用栈信息，默认禁止
+                .build();
+        Printer androidPrinter = new AndroidPrinter();                  // 通过 android.util.Log 打印日志的打印器
+        XLog.init(                                                      // 初始化 XLog
+                config,                                                 // 指定日志配置，如果不指定，会默认使用 new LogConfiguration.Builder().build()
+                androidPrinter);                                        // 添加任意多的打印器。如果没有添加任何打印器，会默认使用 AndroidPrinter(Android)/ConsolePrinter(java)
     }
 
     public static void v(String msg) {
-        XLog.v(getLogEnvironment(STACK), msg);
+        XLog.v(getLogEnvironment(), msg);
     }
 
     public static void d(String msg) {
-        XLog.d(getLogEnvironment(STACK), msg);
+        XLog.d(getLogEnvironment(), msg);
     }
 
     public static void i(String msg) {
-        XLog.i(getLogEnvironment(STACK), msg);
+        XLog.i(getLogEnvironment(), msg);
     }
 
     public static void w(String msg) {
-        XLog.w(getLogEnvironment(STACK), msg);
+        XLog.w(getLogEnvironment(), msg);
     }
 
     public static void e(String msg) {
-        XLog.e(getLogEnvironment(STACK), msg);
+        XLog.e(getLogEnvironment(), msg);
     }
 
     public static void printObject(Object object) {
-        XLog.d(getLogEnvironment(STACK), object);
+        XLog.d(getLogEnvironment(), object);
     }
 
     public static void printJson(String jsonString) {
@@ -57,36 +64,9 @@ public class PLogger {
     /**
      * 拼接打印当前log的环境信息，方便定位
      *
-     * @param stack 回溯栈纵深
      * @return 当前调用日志打印的环境信息
      */
-    private static String getLogEnvironment(int stack) {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        StringBuffer sb = new StringBuffer();
-        if (stackTrace.length > stack) {
-            sb.append(stackTrace[stack - 1].getMethodName());
-            sb.append("_");
-            sb.append(getSimpleName(stackTrace[stack].getClassName()));
-            sb.append("_");
-            sb.append(stackTrace[stack].getMethodName());
-            sb.append("_LineNumber[");
-            sb.append(stackTrace[stack].getLineNumber());
-        }
-        sb.append("]_MainThreadName[");
-        sb.append(Looper.getMainLooper().getThread().getName());
-        sb.append("]_CurrentThreadName[");
-        sb.append(Thread.currentThread().getName());
-        sb.append("]_CurrentPid[");
-        sb.append(Process.myPid());
-        sb.append("]\n%s");
-
-        return sb.toString();
-    }
-
-    private static String getSimpleName(String name) {
-        if (TextUtils.isEmpty(name)) return "";
-        int dot = name.lastIndexOf('.');
-        if (dot != -1) return name.substring(dot + 1);
-        return name;
+    private static String getLogEnvironment() {
+        return "%s";
     }
 }
