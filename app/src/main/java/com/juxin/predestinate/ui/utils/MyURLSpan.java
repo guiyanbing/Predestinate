4e78795f9dabf0400f4ce32d073e396ba809f985
Kind <191350614@qq.com>
2017-06-16 11:23:24 +0800

    fix

--- app/src/main/java/com/juxin/predestinate/ui/utils/MyURLSpan.java
+++ app/src/main/java/com/juxin/predestinate/ui/utils/MyURLSpan.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.juxin.library.request.DownloadListener;
+import com.juxin.predestinate.module.local.mail.MailSpecialID;
 import com.juxin.predestinate.module.logic.application.App;
 import com.juxin.predestinate.module.logic.application.ModuleMgr;
 import com.juxin.predestinate.module.logic.config.Constant;
