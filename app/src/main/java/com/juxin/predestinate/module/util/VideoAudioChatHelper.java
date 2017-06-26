058f5d6cb78a8634ce15aaee3b40e0e7e5863859
WeberGuo <websterguo@eyouv.cn>
2017-06-26 20:12:19 +0800

    单邀addvcid

--- app/src/main/java/com/juxin/predestinate/module/util/VideoAudioChatHelper.java
+++ app/src/main/java/com/juxin/predestinate/module/util/VideoAudioChatHelper.java
@@ -246,6 +246,7 @@ public class VideoAudioChatHelper {
         if (response.isOk()) {
             JSONObject resJo = jo.optJSONObject("res");
             final long vcID = resJo.optLong("vc_id");
+            addvcID(vcID);
             int msgVer = resJo.optInt("confer_msgver");
             Bundle bundle = newBundle(vcID, dstUid, 1, type, msgVer);
             bundle.putInt("vc_girl_type", 1);
