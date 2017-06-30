a0f2c0dea224b98903d955027f3b500f029465c7
sumeng <csudxysm@163.com>
2017-06-30 09:57:47 +0800

    女号邀请弹框

--- app/src/main/java/com/juxin/predestinate/module/util/VideoAudioChatHelper.java
+++ app/src/main/java/com/juxin/predestinate/module/util/VideoAudioChatHelper.java
@@ -154,7 +154,7 @@ public class VideoAudioChatHelper {
      */
     public void inviteVAChat(final Activity context, long dstUid, int type, boolean flag, int singleType, String channel_uid, boolean isInvate) {
         boolean isMan = ModuleMgr.getCenterMgr().getMyInfo().isMan();  // 女号不弹框
-        if (isMan && flag && PSP.getInstance().getInt(ModuleMgr.getCommonMgr().getPrivateKey(Constant.APPEAR_FOREVER_TYPE), 0) == 0) {
+        if (isInvate || (isMan && flag && PSP.getInstance().getInt(ModuleMgr.getCommonMgr().getPrivateKey(Constant.APPEAR_FOREVER_TYPE), 0) == 0)) {
             UIShow.showLookAtHerDlg(context, dstUid, channel_uid, isInvate);
             return;
         }
